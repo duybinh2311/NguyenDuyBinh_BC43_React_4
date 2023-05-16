1451a2dcf1c3170f64de2fbddb9629030046d335
duybinh2311 <duybinh.nguyen2311@gmail.com>
2023-05-16 21:45:59 +0700

    update title

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class Footer extends Component {
   render() {
     return (
-      <div className="text-bg-dark text-center py-3 fs-2 fw-bold">Footer</div>
+      <div className="text-bg-dark text-center py-3 fs-2 fw-bold">BC - 43</div>
     )
   }
 }
